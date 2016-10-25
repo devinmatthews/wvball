f6f8b59c7bd96d8ab0e2d4ed73ac600a53673205
Devin Matthews <devin@devmc.com>
2016-10-25 18:45:05 -0500

    Created initial classes to get started

---
+++ WVB/WVB.Classes/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WVB.Classes
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+    }
+
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Participant> Members { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class Participant
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ParticipantType Type { get; set; }
+    }
+}
---
+++ WVB/WVB.Classes/Enums/ParticipantType.cs
@@ -0,0 +1,10 @@
+﻿namespace WVB.Classes
+{
+    public enum ParticipantType
+    {
+        Player = 1,
+        Manager = 2,
+        Assisstant = 3,
+        Spectator = 4
+    }
+}
